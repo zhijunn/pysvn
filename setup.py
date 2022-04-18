463a15ad33655b7b6b33d4774cc1601ea0cc7ec0
Ryan Bender <ryan.bender.general@gmail.com>
2022-04-18 15:26:27 -0400

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pysubversion",
-    version="0.2.0",
+    version="0.2.1",
     keywords=["svn", "pysvn", "subversion"],
     description="Svn for python.",
     long_description=long_description,
