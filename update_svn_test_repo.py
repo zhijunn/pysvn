bdd8f733e7f22ef8470d59bd5894392bbe8f12fe
Ryan Bender <ryan.bender.general@gmail.com>
2022-04-22 15:37:53 -0400

    lets try this

--- update_svn_test_repo.py
+++ update_svn_test_repo.py
@@ -2,10 +2,10 @@ import subprocess
 import pathlib
 
 def main():
-    subprocess.Popen('ls')
     path = str(pathlib.Path('tests/test_repo').resolve())
     cwd = str(pathlib.Path('tests/test_svn').resolve())
-    subprocess.Popen(f'svn relocate file://{path}', cwd=cwd)
+    filepath = ['svn', 'relocate', f'file://{path}']
+    subprocess.Popen(filepath, cwd=cwd)
 
 if __name__ == '__main__':
     main()
